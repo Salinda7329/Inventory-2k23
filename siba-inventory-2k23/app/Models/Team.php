e9e902f8511a2b6dbffc88f6469cc1afbc0852e0
SalindaGIT <salindakas@gmail.com>
2023-12-01 15:27:54 +0530

    Updated models

--- siba-inventory-2k23/app/Models/Team.php
+++ siba-inventory-2k23/app/Models/Team.php
@@ -33,10 +33,6 @@ class Team extends JetstreamTeam
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'personal_team',
-    ];
 
     /**
      * The event map for the model.
