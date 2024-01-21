c4564f27a2cf0627a9d890a0080b1405398139bb
SalindaGIT <salindakas@gmail.com>
2024-01-21 17:53:29 +0530

    Created Middleware to protect systemAdmin,storeManager routes. Redirect back without roles

--- siba-inventory-2k23/app/Http/Controllers/AdminDashboardController.php
+++ siba-inventory-2k23/app/Http/Controllers/AdminDashboardController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 
 class AdminDashboardController extends Controller
 {
-    public function __construct() {
-        $this->middleware('auth');
-      }
       public function index() {
         return view('systemAdmin.system-admin-home');
       }
