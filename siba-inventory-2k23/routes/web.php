3c79fce0f8c520de068c906723ed4b6597238568
chamal adhikaram <100332002+chamal01@users.noreply.github.com>
2024-01-23 20:08:08 +0530

    ..
    My Changes

--- siba-inventory-2k23/routes/web.php
+++ siba-inventory-2k23/routes/web.php
@@ -8,22 +8,12 @@ use Illuminate\Support\Facades\Route;
 
 // login page
 Route::get('/', function () {
-<<<<<<< Updated upstream
     return view('welcome');
 });
 // home page
 Route::get('/home', function () {
     return view('welcome');
 })->middleware('RedirectToHomeBasedOnRole');
-=======
-    return view('auth.login');
-});
-
-Route::get('/Register' , function(){
-    return view('auth.register');
-});
-
->>>>>>> Stashed changes
 
 //default dashboard by Jetstream
 Route::middleware([
@@ -148,7 +138,6 @@ Route::get('/user/view-store', function () {
     return view('DepartmentUser.visit-store-user');
 });
 
-<<<<<<< Updated upstream
 //-------------------------------------------------End Department user------------------------------------------------
 
 
@@ -158,10 +147,3 @@ Route::get('/accountDeactivated', function () {
     return view('errors.accountDeactivated');
 })->name('accountDeactivated');
 //-------------------------------------------------End Error pages------------------------------------------------
-=======
-//-----------------------------------------Purchasing Manager-------------------------------------------------------------------------
-
-Route::get('/pm/addNewProduct' , function(){
-    return view('PurchasingManager.Add-new-product');
-});
->>>>>>> Stashed changes
