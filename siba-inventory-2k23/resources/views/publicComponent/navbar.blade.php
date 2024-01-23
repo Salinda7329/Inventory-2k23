586493b5b2ae130446f3004b10b67e4bf2afb3d9
SalindaGIT <salindakas@gmail.com>
2024-01-23 18:23:53 +0530

    User name, role displays in home

--- siba-inventory-2k23/resources/views/publicComponent/navbar.blade.php
+++ siba-inventory-2k23/resources/views/publicComponent/navbar.blade.php
@@ -32,8 +32,8 @@ id="layout-navbar"
                 </div>
               </div>
               <div class="flex-grow-1">
-                <span class="fw-semibold d-block">Mr.Sadeera</span>
-                <small class="text-muted">Store Manager</small>
+                <span class="fw-semibold d-block">{{ Auth::user()->getRoleNameAttribute() }}</span>
+                <small class="text-muted">{{ Auth::user()->name }}</small>
               </div>
             </div>
           </a>
