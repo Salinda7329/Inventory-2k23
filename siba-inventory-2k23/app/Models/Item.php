5fca01d81a89bcb6f54bb0ed076e5c3a890bc6b8
SalindaGIT <salindakas@gmail.com>
2024-01-27 21:10:14 +0530

    Fix Brand Name

--- siba-inventory-2k23/app/Models/Item.php
+++ siba-inventory-2k23/app/Models/Item.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
-	protected $fillable = [
+    protected $fillable = [
         'po_no',
         'product_id',
         'brand_id',
@@ -27,6 +27,10 @@ class Item extends Model
     {
         return $this->belongsTo(User::class, 'created_by', 'id');
     }
+    public function conditionUpdatedByUser()
+    {
+        return $this->belongsTo(User::class, 'condition_updated_by', 'id');
+    }
     public function categoryData()
     {
         return $this->belongsTo(Product::class, 'product_id')->select('id', 'product_name', 'category_id');
@@ -37,10 +41,26 @@ class Item extends Model
         return $this->belongsTo(Product::class, 'product_id', 'id');
     }
 
+    public function brandData()
+    {
+        return $this->belongsTo(Brand::class, 'brand_id', 'id');
+    }
+
+    public function getIsCondtionItemAttribute()
+    {
+        $status = [
+            1 => 'Working',
+            2 => 'Damaged',
+            // Add more roles as needed
+        ];
+
+        return $status[$this->attributes['condition']] ?? 'Unknown Status';
+    }
+
     public function getIsActiveItemAttribute()
     {
         $status = [
-            1 =>'Active',
+            1 => 'Active',
             2 => 'Deactivated',
             3 => 'Deleted',
             // Add more roles as needed
