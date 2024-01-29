d06de8403bed746225bbed48f076f0efd488efa4
SalindaGIT <salindakas@gmail.com>
2024-01-29 14:27:25 +0530

    Request item Product name display

--- siba-inventory-2k23/resources/views/DepartmentUser/DUComponent/Modal-RequestItems.blade.php
+++ siba-inventory-2k23/resources/views/DepartmentUser/DUComponent/Modal-RequestItems.blade.php
@@ -11,45 +11,90 @@ auto watenn dann --}}
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalrequest">Modal title</h5>
+                <h5 class="modal-title" id="modalrequest">Request an Item</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="mb-3">
-                    <label for="itemname" class="form-label">Item Name</label>
-                    <input class="form-control" type="text" id="itemname"
-                        placeholder="" readonly />
+                    <label for="product_name" class="form-label">Product Name</label>
+                    <input class="form-control" type="text" id="product_name" placeholder="" readonly />
                 </div>
                 <div class="mb-3">
                     <label for="itemid" class="form-label">Item ID</label>
-                    <input class="form-control" type="text" id="itemid"
-                        placeholder="" readonly />
+                    <input class="form-control" type="text" id="item_id" placeholder="" readonly />
                 </div>
-                    <div class="row g-2">
-                        <div class="col mb-0">
-                            <label for="quentity" class="form-label">Quentity</label>
-                            <input type="text" id="quentity" class="form-control" placeholder="" />
-                        </div>
-                        <div class="col mb-0">
-                            <label for="html5-date-input" class="col-md-2 col-form-label">Date</label>
-                            <div class="col-md-10">
-                              <input class="form-control" type="date" value="2021-06-18" id="html5-date-input" />
-                            </div>
-                        </div>
-                        <div>
-                            <label for="comment" class="form-label">Comment</label>
-                            <textarea class="form-control" id="comment" rows="3"></textarea>
-                          </div>
+                <div class="mb-3">
+                    <label for="itemname" class="form-label">Item Name</label>
+                    <input class="form-control" type="text" id="item_name" placeholder="" readonly />
+                </div>
+                <div class="row g-2">
+                    <div class="col mb-0">
+                        <label for="quentity" class="form-label">Quentity</label>
+                        <input type="text" id="quentity" class="form-control" placeholder="" />
+                    </div>
+                    <div>
+                        <label for="comment" class="form-label">Comment</label>
+                        <textarea class="form-control" id="comment" rows="3"></textarea>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="button" class="btn btn-primary">request</button>
-                </div>
             </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                    Close
+                </button>
+                <button type="button" class="btn btn-primary">request</button>
+            </div>
+            <script>
+                $(document).ready(function() {
+
+                    //edit user button
+                    $(document).on('click', '.requestItemButton', function(e) {
+                        e.preventDefault();
+                        let item_Id = $(this).attr('id');
+                        // alert(id);
+
+                        $.ajax({
+                            url: '{{ route('item.edit') }}',
+                            method: 'get',
+                            data: {
+                                item_Id: item_Id,
+                                _token: '{{ csrf_token() }}'
+                            },
+                            success: function(response) {
+
+                                console.log(response.po_no);
+                                // Set id value to the hidden field
+                                $('#item_id').val(response.id);
+                                $('#item_name').val(response.item_name);
+                                // Fetch product name using product_id
+                                fetchProductName(response.product_id);
+                            }
+
+
+                        });
+
+
+                    })
+
+
+                    // Function to fetch product name using product_id
+                    function fetchProductName(productId) {
+                        $.ajax({
+                            url: '{{ route('fetchProductDetails') }}',
+                            method: 'get',
+                            data: {
+                                productId: productId,
+                                _token: '{{ csrf_token() }}'
+                            },
+                            success: function(response) {
+                                $('#product_name').val(response.product_name);
+                            }
+                        });
+                    }
+                });
+            </script>
         </div>
     </div>
+</div>
 </div>
 </div>
