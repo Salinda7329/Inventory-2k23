deca8241a0d389dadd994754df1a7bf827695fda
SalindaGIT <salindakas@gmail.com>
2024-02-02 14:54:18 +0530

    Created SystemAdminSeeder to store default System Admin credentials

---
+++ siba-inventory-2k23/database/seeders/SystemAdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SystemAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'email' => 'admin@mail.com',
+            'name' => 'Admin Amal',
+            'epf' => '0000',
+            'role' => '5',
+            'dept' => 'NULL',
+            'password' => Hash::make('admin@mail.com'),
+        ]);
+    }
+}
